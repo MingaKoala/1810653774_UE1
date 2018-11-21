7f99cdcb9f17fa3ac93a666a9f8c94fa4b6c5f6a
MingaKoala <44052264+MingaKoala@users.noreply.github.com>
2018-11-21 18:18:31 +0100

    Aufgabe 2

---
+++ src/Aufgabe2_1810653774.java
@@ -0,0 +1,19 @@
+public class Aufgabe2_1810653774 {
+
+    public static void main(String[] args) {
+
+        String Name = "Bene"; /*  repräsentiert unveränderliche Zeichenketten */
+        int Zahl = 69; /* entspricht -2147483648 ... 2147483647 */
+        double y = 6.9; /* doppelte genauigkeit */
+        boolean Wahrheitswert = true; /* dient zum Speichern der logischen Werte true und false */
+        long a =77L; /* der Datentyp long ist die erweiterte Form des int-Typs. Im Gegensatz zum int hat dieser Datentyp 8 Bytes */
+        char c = 67; /*Der Datentyp char ist ein elementarer Typ Er umfaßt Werte von 0 bis 65535, die meist als Kennzahlen für Zeichen interpretiert werden */
+        byte b = 44; /* geht nur bis -128 und 127 */
+        short s = 22; /* -32768 ... 32767 */
+        float f = 11; /* einfache genauigkeit */
+
+
+        final int x = 218; /* Variable sind veraenderbar Konstante nicht mehr */
+    }
+
+}
---
+++ src/Aufgabe3_1810653774.java
@@ -0,0 +1,31 @@
+public class Aufgabe3_1810653774 {
+    public static void main(String[] args) {
+
+        int zahl1 = 4;
+        int zahl2 = 6;
+        int zahl3 = 2;
+        double zahl4 = 12.5;
+        double zahl5 = 10.5;
+        double zahl6 = 2.5;
+
+        /* Rechnungen */
+
+        double a = zahl1+zahl2;
+        double b = zahl1/zahl3;
+        double c = zahl4*zahl2;
+        double d = zahl1%zahl2;
+        zahl3++;
+        zahl2--;
+
+
+
+        /* Ausgabe der Operatoren in der Konsole */
+        System.out.println("4 +  6=" +a);
+        System.out.println("4 / 2=" +b);
+        System.out.println("12.5 * 6=" +c);
+        System.out.println("4 % 6" +d);
+        System.out.println("Inkrementieren Sie 2 = " +zahl3++);
+        System.out.println("dekrementieren sie 6 = " +zahl2--);
+        System.out.println("____________________________");
+    }
+}
