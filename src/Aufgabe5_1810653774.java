6c0f5357f0e176f2f4acfb1d3f44ecd39afe45cf
MingaKoala <44052264+MingaKoala@users.noreply.github.com>
2018-11-21 18:55:35 +0100

    Aufgabe 5

---
+++ src/Aufgabe5_1810653774.java
@@ -0,0 +1,18 @@
+public class Aufgabe5_1810653774 {
+    public static void main(String[] args) {
+
+        int zahl1 = 4;
+        int zahl2 = 2;
+
+
+
+
+        System.out.println(zahl1++);
+        System.out.println(zahl2++);
+        System.out.println(zahl1);
+        System.out.println(zahl2);
+        System.out.println(++zahl1);
+        System.out.println(++zahl2);
+
+    }
+}
