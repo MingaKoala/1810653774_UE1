4f1dfc909cf41ea56893de5c9d1c3e9d1437c01c
MingaKoala <44052264+MingaKoala@users.noreply.github.com>
2018-11-21 18:40:55 +0100

    Aufgabe 4

---
+++ src/Aufgabe4_1810653774.java
@@ -0,0 +1,19 @@
+public class Aufgabe4_1810653774 {
+
+    public static void main(String[] args) {
+
+        int zahl1 = 4;
+        int zahl2 = 2;
+
+        System.out.println(zahl1<zahl2);
+        System.out.println(zahl2<zahl1);
+        System.out.println(zahl1>zahl2);
+        System.out.println(zahl2>zahl1);
+        System.out.println(zahl1<=zahl2);
+        System.out.println(zahl2<=zahl1);
+        System.out.println(zahl1>=zahl2);
+        System.out.println(zahl2>=zahl1);
+        System.out.println(zahl1==zahl2);
+        System.out.println(zahl1!=zahl2);
+    }
+}
